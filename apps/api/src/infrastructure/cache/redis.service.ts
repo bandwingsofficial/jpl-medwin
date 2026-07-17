1649888382693d5e7696f16f873eacc45fb5d8ff
bandwingsofficial <bandwingsofficial@gmail.com>
2026-07-17 17:34:33 +0530

    Add Redis authentication parameters to Redis client

--- apps/api/src/infrastructure/cache/redis.service.ts
+++ apps/api/src/infrastructure/cache/redis.service.ts
@@ -9,6 +9,8 @@ export class RedisService implements OnModuleDestroy {
     this.client = new Redis({
       host: process.env.REDIS_HOST,
       port: Number(process.env.REDIS_PORT),
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
     });
   }
 
