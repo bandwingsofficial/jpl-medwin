d1b5830fb4ec74332f3ad25317c52d04f8759f62
bandwingsofficial <bandwingsofficial@gmail.com>
2026-07-17 17:48:59 +0530

    Update redis.module.ts

--- apps/api/src/modules/auth/infrastructure/redis/redis.module.ts
+++ apps/api/src/modules/auth/infrastructure/redis/redis.module.ts
@@ -4,20 +4,20 @@ import Redis from 'ioredis';
 import { REDIS } from './redis.constants';
 import { RedisService } from './redis.service';
 
-@Global() // ✅ makes it available everywhere
+@Global()
 @Module({
   providers: [
     {
       provide: REDIS,
       useFactory: () => {
-        return new Redis({
-          host: 'localhost',
-          port: 6379,
+        return new Redis(process.env.REDIS_URL!, {
+          maxRetriesPerRequest: null,
+          enableReadyCheck: true,
         });
       },
     },
     RedisService,
   ],
-  exports: [REDIS, RedisService], // ✅ export both
+  exports: [REDIS, RedisService],
 })
 export class RedisModule {}
